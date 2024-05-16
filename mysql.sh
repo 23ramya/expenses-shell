b4821e8e121464e9f8e34aa45ce4514799152f57
ramya <ramya.pekatanoju@gmail.com>
2024-05-16 23:20:23 +0530

    adding project

--- mysql.sh
+++ mysql.sh
@@ -45,12 +45,12 @@ else
          #Below code will be useful for idempotent nature 
          mysql -h db.ramya.cloud -uroot -p${mysql_root_password} -e 'show databases;' &>>LOGFILE
 
-         if [ $? -ne 0 ]
+         #if [ $? -ne 0 ]
 
-         then
+         #then
 
-          mysql_secure_installation --set-root-pass ${mysql_root_password}  &>>LOGFILE
-          VALIDATE $? "MYSQL ROOT PASSWORD SETUP"
+         # mysql_secure_installation --set-root-pass ${mysql_root_password}  &>>LOGFILE
+         # VALIDATE $? "MYSQL ROOT PASSWORD SETUP"
 
           else
           echo -e "MYSQL root password is already setup ...$Y SKIPPING $N"
