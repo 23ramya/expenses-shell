f925bc19a9c52caf18cf7121aa31d1486d6327b1
ramya <ramya.pekatanoju@gmail.com>
2024-05-16 22:47:40 +0530

    adding project

--- mysql.sh
+++ mysql.sh
@@ -50,7 +50,7 @@ else
          then
 
           mysql_secure_installation --set-root-pass ${mysql_root_password}  &>>LOGFILE
-          VALIDATE $? "MYSQL ROOT PASSWORD setup"
+          VALIDATE $? "MYSQL ROOT PASSWORD SETUP"
 
           else
           echo -e "MYSQL root password is already setup ...$Y SKIPPING $N"
